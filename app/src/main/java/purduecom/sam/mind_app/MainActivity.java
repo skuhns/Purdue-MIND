7546442bf39b577abb847ef6876f774a863b3e4f
skuhns <skuhns@purdue.edu>
2017-02-26 19:10:40 -0500

    first commit

---
+++ app/src/main/java/purduecom/sam/mind_app/MainActivity.java
@@ -0,0 +1,43 @@
+package purduecom.sam.mind_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button gameActivity;
+    Button calibrateActivity;
+    Button exerciseActivity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gameActivity = (Button) findViewById(R.id.button);
+        calibrateActivity = (Button) findViewById(R.id.button2);
+        exerciseActivity = (Button) findViewById(R.id.button3);
+
+        gameActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+
+        calibrateActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, exercise_Activity.class));
+            }
+        });
+
+        exerciseActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Calibrate_Activity.class));
+            }
+        });
+
+    }
+}
