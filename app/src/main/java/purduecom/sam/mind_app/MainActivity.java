96660f16f94bac9e4b69e7e8619ebbc29c055106
skuhns <skuhns@purdue.edu>
2017-04-04 14:47:10 -0400

    file storage and recall

--- app/src/main/java/purduecom/sam/mind_app/MainActivity.java
+++ app/src/main/java/purduecom/sam/mind_app/MainActivity.java
@@ -14,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
+
         setContentView(R.layout.activity_main);
         gameActivity = (Button) findViewById(R.id.button);
         calibrateActivity = (Button) findViewById(R.id.button2);
